16fb154b807e5fbf510a52ecd3edf07b3ff2dfbe
faiyazuddeen <m.faiyaz123@gmail.com>
2018-11-27 16:55:19 +0530

    admin tasks service and model

--- src/models/admin-tasks.model.js
+++ src/models/admin-tasks.model.js
@@ -2,14 +2,23 @@
 // 
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
+
+const admintasks = require('../services/admin-tasks/admin-tasks');
+
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const { Schema } = mongooseClient;
   const adminTasks = new Schema({
-    text: { type: String, required: true }
-  }, {
-    timestamps: true
-  });
+    userId: {
+      type: Schema.ObjectId,
+      ref: 'User'
+    },
+    tasks: {
+      type: Array,
+      default: admintasks
+    }
+  },
+  { timestamps: true });
 
   return mongooseClient.model('adminTasks', adminTasks);
 };
---
+++ src/services/admin-tasks/admin-tasks.js
@@ -0,0 +1,58 @@
+const adminTasks = [
+    {
+        id: 1,
+        taskName: 'Helpline',
+        selected: false
+    }, {
+        id: 2,
+        taskName: 'Feed Commercials',
+        selected: false
+    }, {
+        id: 3,
+        taskName: 'Feed Contents',
+        selected: false
+    }, {
+        id: 4,
+        taskName: 'Approve NGO News',
+        selected: false
+    }, {
+        id: 5,
+        taskName: 'Trending News',
+        selected: false
+    }, {
+        id: 6,
+        taskName: 'NGO Verifications',
+        selected: false
+    }, {
+        id: 7,
+        taskName: 'Users/Notifications',
+        selected: false
+    }, {
+        id: 8,
+        taskName: 'Petitions and RTIs',
+        selected: false
+    }, {
+        id: 9,
+        taskName: 'Reported posts',
+        selected: false
+    }, {
+        id: 10,
+        taskName: 'Statistics',
+        selected: false
+    }, {
+        id: 11,
+        taskName: 'Suggestions/Feedback',
+        selected: false
+    }, {
+        id: 12,
+        taskName: 'Abuse Reports',
+        selected: false
+    }, {
+        id: 13,
+        taskName: 'About us',
+        selected: false
+    }
+]
+
+
+module.exports = adminTasks;
